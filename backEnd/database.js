3dffd68f8423a45b1e82fa0afc888e193f9c5ab3
Leonardo Dornelles Nunes <leonardonunesdornelles@gmail.com>
2024-06-25 14:17:00 -0300

    adding simple connection with database testdb

--- backEnd/database.js
+++ backEnd/database.js
@@ -1,1 +1,19 @@
-const mysql = require("mysql");
+const mysql = require("mysql2/promise");
+const myData = require("./private.js");
+
+async function testFunction() {
+
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: myData.dataBaseUser,
+        password: myData.dataBasePassword,
+        database: "testdb",
+    });
+
+    const response = await connection.query("show databases;");
+
+    console.log(typeof response);
+
+}
+
+testFunction();
